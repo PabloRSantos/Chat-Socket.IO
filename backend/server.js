41f20f9eb690786f88f7e9dc59a39db19fb3f778
PabloRSantos <pablorsantos15@gmail.com>
2020-07-21 23:14:49 -0300

    ...

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,18 @@ const app = express()
 const server = require("http").createServer(app)
 const io = require("socket.io")(server)
 
-app.get("/", (req, res) => {
-
-})
 
 let messages = []
 
 io.on("connection", socket => {
 
+
     socket.emit("previousMessages", messages)
 
     socket.on("sendMessage", data => {
-
+        data.key = messages.length
         //emit envia so pro socket que conectou
+        console.log(data)
         messages.push(data)
         socket.broadcast.emit("receivedMessage", data) //envia para todos os sockets conectados
     })
